31eee98c1b428887b545e5bc6e3ab1cad2e0dadc
Diana Portela <dianasaddi@gmail.com>
2024-08-19 13:12:56 -0300

    stupid coaching, challenge

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  root to: 'pages#home'
+
+  get 'ask', to: 'pages#ask'
+  get 'answer', to: 'pages#answer'
 end
